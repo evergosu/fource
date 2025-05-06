8fa2415a463b46e8c7ec4cca319e4b1bfb55dc19
Evergosu <evergosu@gmail.com>
2025-05-06 11:09:17 +0300

    Replaced mock data with real fetch.

--- src/client/src/app/page.tsx
+++ src/client/src/app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-  return <main>hi</main>;
+import { relativeFetch } from 'src/lib/fetch';
+
+export default async function Home() {
+  const data = await relativeFetch('/api/story');
+
+  const story: string = (await data.json()) as string;
+
+  return <main>{story}</main>;
 }
