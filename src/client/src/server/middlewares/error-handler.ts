27bc514b169d0ea145e19d88ba66fb90547c3899
evergosu <evergosu@gmail.com>
2025-05-29 15:36:53 +0300

    Added custom error handler middleware.

---
+++ src/client/src/server/middlewares/error-handler.ts
@@ -0,0 +1,12 @@
+import type { ErrorRequestHandler } from 'express';
+import type { Logger } from 'client/lib/logger';
+
+export function createErrorHandler(logger: Logger): ErrorRequestHandler {
+  return (error, _request, response) => {
+    logger.error('Express error.', error);
+
+    if (!response.headersSent) {
+      response.status(500).json({ error: 'Internal NextJS server error.' });
+    }
+  };
+}
