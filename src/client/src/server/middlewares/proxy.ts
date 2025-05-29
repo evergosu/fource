0969f1b8831a801cf30f0e18a830bda331c57c3d
evergosu <evergosu@gmail.com>
2025-05-29 15:46:36 +0300

    Added proxy middleware for /api routes, because backend server responsibility.

---
+++ src/client/src/server/middlewares/proxy.ts
@@ -0,0 +1,30 @@
+import type { Logger } from 'client/lib/logger';
+import type { RequestHandler } from 'express';
+
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
+export function createApiProxy(
+  target: URL,
+  logger: Logger,
+): [string, RequestHandler] {
+  logger.info(`Proxy /api → ${target.origin}/api`);
+
+  return [
+    '/api/',
+    createProxyMiddleware({
+      on: {
+        proxyReq: (proxyRequest, request) => {
+          logger.info(
+            `[PROXY] ${request.method} ${request.originalUrl} → ${proxyRequest.protocol}//${proxyRequest.host}${proxyRequest.path}`,
+          );
+        },
+        error: error => {
+          logger.error('Error during proxy request.', error);
+        },
+      },
+      target: `${target.origin}/api/`,
+      changeOrigin: true,
+      autoRewrite: true,
+    }),
+  ];
+}
