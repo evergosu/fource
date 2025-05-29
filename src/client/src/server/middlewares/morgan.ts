9d353650bbb530f98cd38f7e0eee31b0869e3497
evergosu <evergosu@gmail.com>
2025-05-29 15:41:15 +0300

    Added morgan middleware to log http requests.

---
+++ src/client/src/server/middlewares/morgan.ts
@@ -0,0 +1,16 @@
+import type { RequestHandler } from 'express';
+
+import { getEnvironment } from 'client/lib/environment';
+import morgan from 'morgan';
+
+const environment = getEnvironment();
+
+const isProduction = environment.node === 'production';
+
+export const morganByEnvironment: RequestHandler[] = [
+  isProduction
+    ? morgan('combined', {
+        skip: (_, response) => response.statusCode < 400,
+      })
+    : morgan('dev'),
+];
