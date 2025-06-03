2a815e2663b8251a05ae7fcb85a23385cbdce085
evergosu <evergosu@gmail.com>
2025-06-03 16:15:38 +0300

    Added helper to provide test context to cucumber steps.

---
+++ src/tests/src/lib/connect-repository.ts
@@ -0,0 +1,11 @@
+import type { Repository } from 'server/story/story';
+import type { TestContext } from 'vitest';
+
+export function connectRepository<T extends Repository>(
+  repository: new (
+    ...parameters: ConstructorParameters<typeof Repository>
+  ) => T,
+  context: unknown,
+): T {
+  return new repository((context as TestContext).database);
+}
