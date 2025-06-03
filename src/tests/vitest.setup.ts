43fd490f504d235d2c8ff951e23355e2d15f8ad3
evergosu <evergosu@gmail.com>
2025-06-03 16:21:57 +0300

    Configured bdd tests to work in full setup and respect libraries.

--- src/tests/vitest.setup.ts
+++ src/tests/vitest.setup.ts
@@ -1,1 +1,106 @@
 import '@testing-library/jest-dom/vitest';
+
+import type { DatabaseContext } from 'server/database/clients/client';
+
+import {
+  type ServerContext as ApplicationServerContext,
+  startServer as startApplicationServer,
+} from 'server/server/express';
+import {
+  type ServerContext as NextServerContext,
+  startServer as startNextServer,
+} from 'client/server/express';
+import {
+  type RunnerTaskBase,
+  type TestContext,
+  beforeEach,
+  beforeAll,
+  afterAll,
+} from 'vitest';
+import { createPostgresLiteContext } from 'server/database/clients/pglite';
+import { Logger as ServerLogger } from 'server/lib/logger';
+import { Logger as ClientLogger } from 'client/lib/logger';
+
+vi.mock('next/font/google', () => ({
+  Inter: () => ({
+    variable: '--font-inter',
+    className: 'font-inter',
+  }),
+}));
+
+let database: DatabaseContext;
+let server: ApplicationServerContext;
+let nextServer: NextServerContext;
+
+beforeAll(async () => {
+  const serverLogger = new ServerLogger({
+    style: 'colorful',
+    level: 'success',
+  });
+
+  database = await createPostgresLiteContext(serverLogger);
+
+  server = startApplicationServer(database, serverLogger, 'random');
+
+  await database.truncateAll();
+
+  const clientLogger = new ClientLogger({
+    style: 'colorful',
+    level: 'success',
+  });
+
+  try {
+    nextServer = await startNextServer(
+      clientLogger,
+      new URL(`http://localhost:${server.getPort()}`),
+      'random',
+    );
+  } catch (error: unknown) {
+    clientLogger.error('Error during NextJS start.', error);
+  }
+
+  const url = `http://localhost:${nextServer.getPort().toString()}`;
+
+  process.env.NEXT_PUBLIC_ORIGIN = url;
+  process.env.NEXT_PUBLIC_BASE_URL = url;
+});
+
+afterAll(async () => {
+  await nextServer.shutdown('tests done');
+  await server.shutdown('tests done');
+});
+
+beforeEach<TestContext>(async context => {
+  const tag = '@database';
+
+  const hasTag = (task: RunnerTaskBase): boolean => {
+    if (task.name.includes(tag)) return true;
+
+    let current: RunnerTaskBase = context.task;
+
+    while (hasSuite(current)) {
+      if (current.suite.name.includes(tag)) return true;
+
+      current = current.suite;
+    }
+
+    return false;
+  };
+
+  // Truncate all database tables and provide context,
+  // if any entity in test has tag in the name and
+  // task is the first step of current scenario.
+  if (hasTag(context.task) && isFirstStepOfScenario(context.task)) {
+    await database.truncateAll();
+
+    context.database = database.getClient();
+  }
+});
+
+function isFirstStepOfScenario(task: RunnerTaskBase) {
+  return hasSuite(task) ? task.suite.tasks.at(0)?.name === task.name : false;
+}
+
+function hasSuite(task: unknown): task is { suite: RunnerTaskBase } {
+  return !!task && typeof task === 'object' && 'suite' in task;
+}
