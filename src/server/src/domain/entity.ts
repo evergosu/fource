be616d5c20c8d5548ef6822ab2beaee7412030c9
evergosu <evergosu@gmail.com>
2025-06-12 15:54:23 +0300

    Created domain class to make unique entities of business model.

---
+++ src/server/src/domain/entity.ts
@@ -0,0 +1,69 @@
+import { UniqueIdentifier } from './unique-identifier';
+
+/**
+ * Base class representing a domain entity with a unique identity.
+ *
+ * `Entities` are defined by their identity (ID), not just their properties.
+ * Two entities are considered equal if they share the same ID, regardless of their properties.
+ *
+ * @template `T` is the shape of the entity's properties.
+ */
+export abstract class Entity<T> {
+  /**
+   * The unique identity of this entity.
+   */
+  protected readonly _id: UniqueIdentifier;
+
+  /**
+   * Constructs a new `Entity` instance.
+   *
+   * @param properties - The entity's domain properties.
+   * @param id - An optional pre-defined unique identifier. If not provided, a new one will be generated.
+   */
+  constructor(
+    public readonly properties: T,
+    id?: UniqueIdentifier,
+  ) {
+    this._id = id ?? UniqueIdentifier.create();
+    this.properties = properties;
+
+    Object.freeze(this);
+  }
+
+  /**
+   * Returns the entity's unique identifier.
+   */
+  public get id(): UniqueIdentifier {
+    return this._id;
+  }
+
+  /**
+   * Type guard to check whether a given value is an instance of an `Entity`.
+   *
+   * @param value - The value to check.
+   * @returns `true` if the value is an `Entity`, `false` otherwise.
+   */
+  public static isEntity(value: unknown): value is Entity<unknown> {
+    return value instanceof Entity;
+  }
+
+  /**
+   * Compares this `Entity` with another to determine equality.
+   *
+   * @param entity - The other `Entity` to compare against.
+   * @returns `true` if unique identifiers match, `false` otherwise.
+   */
+  public equals(entity?: Entity<unknown>): boolean {
+    if (!entity) return false;
+
+    if (this === entity) {
+      return true;
+    }
+
+    if (!Entity.isEntity(entity)) {
+      return false;
+    }
+
+    return this._id.equals(entity._id);
+  }
+}
