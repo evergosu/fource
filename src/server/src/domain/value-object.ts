a9c1a4b649a6818b48580bdd1f280e0453559099
evergosu <evergosu@gmail.com>
2025-06-12 18:03:42 +0300

    Created domain class to make structure unique value objects to express business model.

---
+++ src/server/src/domain/value-object.ts
@@ -0,0 +1,49 @@
+import { deepCompare } from './utility/deep-compare';
+import { deepFreeze } from './utility/deep-freeze';
+
+type Properties = Record<string, unknown>;
+
+/**
+ * Base class representing a `ValueObject` with a structural definition.
+ *
+ * `ValueObjects` are defined by their properties rather than identity.
+ * Two `ValueObjects` are considered equal if all their properties are equal.
+ *
+ * @template T is a shape representing the structure of the `ValueObject`'s properties.
+ */
+export abstract class ValueObject<T extends Properties> {
+  /**
+   * Constructs a new `ValueObject` instance.
+   *
+   * @param properties - The structural properties that define the `ValueObject`.
+   */
+  constructor(public readonly properties: T) {
+    this.properties = deepFreeze({ ...properties });
+  }
+
+  /**
+   * Type guard to check whether a given value is an instance of an `ValueObject`.
+   *
+   * @param value - The value to check.
+   * @returns `true` if the value is an `ValueObject`, `false` otherwise.
+   */
+  public static isValueObject(value: unknown): value is ValueObject<never> {
+    return value instanceof ValueObject;
+  }
+
+  /**
+   * Compares this `ValueObject` with another for structural equality.
+   *
+   * @param value - The other `ValueObject` instance to compare against.
+   * @returns `true` if all properties match, `false` otherwise.
+   */
+  public equals(value?: ValueObject<T>): boolean {
+    if (!value) return false;
+
+    if (this === value) return true;
+
+    if (!(value instanceof ValueObject)) return false;
+
+    return deepCompare(this.properties, value.properties);
+  }
+}
