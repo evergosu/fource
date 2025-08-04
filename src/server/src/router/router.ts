69baf92c53d624bba3341cf9f30cce5b87ea917d
evergosu <evergosu@gmail.com>
2025-08-04 14:32:30 +0300

    Placed application code under eponymous folder.

--- src/server/src/router/router.ts
+++ src/server/src/router/router.ts
@@ -2,7 +2,7 @@ import type { DatabaseContext } from 'server/database/clients/client';
 
 import express, { type Response, type Request, type Router } from 'express';
 
-import { NoStoriesError, Stories } from '../story/story';
+import { NoStoriesError, Stories } from '../application/story/story';
 import { asyncHandler } from './async-handler';
 
 /**
