d2a370e1400b2985aef28e4cad5d07dadbfa2d78
evergosu <evergosu@gmail.com>
2025-08-14 14:52:58 +0300

    Fixed typo.

--- src/server/src/library/ddd/errors.ts
+++ src/server/src/library/ddd/errors.ts
@@ -9,5 +9,5 @@ export * from './application/application-error';
 export * from './domain/identifiers/identifier-errors';
 export * from './domain/domain-error';
 
-export * from './issues//exception';
+export * from './issues/exception';
 export * from './issues/failure';
