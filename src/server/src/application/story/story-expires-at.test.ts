974ca3be4d9534832ff8866e869b7b04658f50a6
evergosu <evergosu@gmail.com>
2025-12-24 16:25:19 +0300

    Changed to use flatMapWiden to propagate error types, improved tests.

--- src/server/src/application/story/story-expires-at.test.ts
+++ src/server/src/application/story/story-expires-at.test.ts
@@ -1,23 +1,33 @@
 import { StoryExpiresTimeNotMatchTTLFailure } from './story-failures';
+import { StoryTimeToLive } from './story-time-to-live';
 import { StoryCreatedAt } from './story-created-at';
 import { StoryExpiresAt } from './story-expires-at';
 
 describe('story expires at', () => {
+  const TWENTY_FOUR_HOURS = 24 * 60 * 60 * 1000;
+
+  const timeToLive =
+    StoryTimeToLive.fromUnixMilliSeconds(TWENTY_FOUR_HOURS).value;
+
   it('should create successfully from createdAt', () => {
     const createdAt = StoryCreatedAt.fromISOString('2025-08-12T10:15:30.000Z');
 
     const expiresAt = StoryExpiresAt.fromCreatedAt(createdAt.value);
 
-    const timeToLive = new Date(StoryExpiresAt['timeToLive']);
-
     const expiryFromTimeToLive = new Date(
-      createdAt.value.toUnixMilliSeconds() + timeToLive.getTime(),
+      createdAt.value.toUnixMilliSeconds() + timeToLive.toUnixMilliSeconds(),
     );
 
     expect(expiresAt.isSuccess).toBe(true);
     expect(expiresAt.value.toISOString()).toBe(
       expiryFromTimeToLive.toISOString(),
     );
+  });
+
+  it('should throw if validators has not been passed', () => {
+    expect(() =>
+      StoryExpiresAt.fromISOString('2025-08-10T10:15:30.000Z'),
+    ).toThrow();
   });
 
   it('should fail if before createdAt', () => {
@@ -51,10 +61,8 @@ describe('story expires at', () => {
   it('should fail if more than time to live after createdAt', () => {
     const createdAtDate = new Date('2025-08-12T10:15:30.000Z');
 
-    const timeToLive = new Date(StoryExpiresAt['timeToLive']);
-
     const dateAfterTimeToLive = new Date(
-      createdAtDate.getTime() + timeToLive.getTime() + 1000,
+      createdAtDate.getTime() + timeToLive.toUnixMilliSeconds() + 1000,
     );
 
     const createdAt = StoryCreatedAt.fromDate(createdAtDate);
--- src/server/src/application/story/story-expires-at.ts
+++ src/server/src/application/story/story-expires-at.ts
@@ -1,19 +1,26 @@
-import { Result, Time } from 'server/library/ddd/primitives';
+import {
+  type FromUnixMsFailures,
+  Result,
+  Time,
+} from 'server/library/ddd/primitives';
 
 import { StoryExpiresTimeNotMatchTTLFailure } from './story-failures';
+import { StoryTimeToLive } from './story-time-to-live';
 import { StoryCreatedAt } from './story-created-at';
 
+const TWENTY_FOUR_HOURS = 24 * 60 * 60 * 1000;
+
+const timeToLive = StoryTimeToLive.fromUnixMilliSeconds(TWENTY_FOUR_HOURS);
+
 /**
- * Value Object representing the expiration time of a Story.
+ * `Value Object` representing the expiration time of a `Story`.
  *
  * Invariants:
  * - must be a valid date/time.
- * - must be strictly after the Story's creation time.
- * - typically set to exactly {@link StoryExpiresAt.timeToLive} after {@link StoryCreatedAt}.
+ * - must be strictly after the `Story`'s creation time.
+ * - typically set to exactly `timeToLive` after `StoryCreatedAt`.
  */
 export class StoryExpiresAt extends Time<StoryExpiresAt> {
-  private static timeToLive = 24 * 60 * 60 * 1000;
-
   private constructor(value: Date) {
     super(value);
   }
@@ -21,40 +28,50 @@ export class StoryExpiresAt extends Time<StoryExpiresAt> {
   /**
    * Creates a new `StoryExpiresAt` instance from a provided date/time.
    * @param date - The date object.
-   * @param validators - The StoryCreatedAt instance for validation.
+   * @param validators - The `StoryCreatedAt` instance for validation.
    * @returns `Result` wrapping the new `StoryExpiresAt` or a:
    * - StoryExpiresTimeNotMatchTTLFailure
+   * - FromUnixMsFailures
    */
-  public static internalCreate(
+  public static _internalCreate(
     date: Date,
     validators: [createdAt: StoryCreatedAt],
-  ): Result<StoryExpiresAt, StoryExpiresTimeNotMatchTTLFailure> {
+  ): Result<
+    StoryExpiresAt,
+    StoryExpiresTimeNotMatchTTLFailure | FromUnixMsFailures
+  > {
     const expiresAt = new StoryExpiresAt(date);
 
     const [createdAt] = validators;
 
-    const expiry = createdAt.addMilliSeconds(StoryExpiresAt.timeToLive).value;
-
-    return Result.fromBoolean(
-      expiresAt.equals(expiry),
-      new StoryExpiresTimeNotMatchTTLFailure(),
-      expiresAt,
-    );
+    return timeToLive
+      .flatMap(ttl =>
+        ttl.addMilliSeconds(createdAt.toUnixMilliSeconds(), validators),
+      )
+      .flatMapWiden(expiry =>
+        Result.fromBoolean(
+          expiresAt.equals(expiry),
+          new StoryExpiresTimeNotMatchTTLFailure(),
+          expiresAt,
+        ),
+      );
   }
 
   /**
-   * Creates a `StoryExpiresAt` exactly {@link StoryExpiresAt.timeToLive} after a given `StoryCreatedAt`.
+   * Creates a `StoryExpiresAt` exactly `timeToLive` after a given `StoryCreatedAt`
    * @param createdAt - The creation time of the story.
-   * @returns `Result` wrapping the new `StoryExpiresAt` set to {@link StoryExpiresAt.timeToLive} after creation or a:
-   * - StoryExpiresTimeNotMatchTTLFailure
+   * @returns `Result` wrapping the new `StoryExpiresAt` set to `timeToLive` after creation or a:
+   * - StoryExpiresTimeNotMatchTTLFiailure
+   * - FromUnixMsFailures
    */
   public static fromCreatedAt(
     createdAt: StoryCreatedAt,
-  ): Result<StoryExpiresAt, StoryExpiresTimeNotMatchTTLFailure> {
-    const expiry = createdAt.addMilliSeconds(StoryExpiresAt.timeToLive, [
-      createdAt,
-    ]);
-
-    return this.internalCreate(expiry.value.toDate(), [createdAt]);
+  ): Result<
+    StoryExpiresAt,
+    StoryExpiresTimeNotMatchTTLFailure | FromUnixMsFailures
+  > {
+    return timeToLive
+      .flatMap(ttl => ttl.addMilliSeconds(createdAt.toUnixMilliSeconds()))
+      .flatMapWiden(t => this.fromDate(t.toDate(), [createdAt]));
   }
 }
