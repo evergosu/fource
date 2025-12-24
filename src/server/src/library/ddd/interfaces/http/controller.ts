731102dfe74f3de88e7e86312ab745c3d8f3e883
evergosu <evergosu@gmail.com>
2025-12-24 16:29:34 +0300

    Changed fold handlers order to match recent updates.

--- src/server/src/library/ddd/interfaces/http/controller.ts
+++ src/server/src/library/ddd/interfaces/http/controller.ts
@@ -86,8 +86,8 @@ export abstract class Controller<
     onSuccessStatus = 200,
   ): [number, unknown] {
     return result.fold(
-      () => this.handleError(result.error),
       () => [onSuccessStatus, result.value],
+      () => this.handleError(result.error),
     );
   }
 
