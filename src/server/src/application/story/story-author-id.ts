d43ebb6e8638fabc4c4be328e953b32d0c16d0c1
evergosu <evergosu@gmail.com>
2025-12-30 14:39:22 +0300

    Added story author identifier as value object to represent the ownage of a story.

---
+++ src/server/src/application/story/story-author-id.ts
@@ -0,0 +1,34 @@
+import {
+  UniqueIdentifier,
+  ValueObject,
+  Guard,
+} from 'server/library/ddd/primitives';
+
+type Properties = Record<'authorId', string>;
+
+/**
+ * Represents a validated, non-empty author identifier of the `Story`.
+ */
+export class StoryAuthorId extends ValueObject<Properties> {
+  private constructor(properties: Properties) {
+    super(properties);
+  }
+
+  /**
+   * Creates a new `StoryAuthorId` value object.
+   * @param authorId - The raw title string.
+   * @returns `Result` with a `StoryAuthorId` or an `ApplicationFailure`.
+   */
+  public static create(authorId: string) {
+    return Guard.againstNullOrUndefined(authorId, 'authorId')
+      .flatMapWiden(() => UniqueIdentifier.create(authorId))
+      .map(id => new StoryAuthorId({ authorId: id.toString() }));
+  }
+
+  /**
+   * The underlying string value of the author identifier.
+   */
+  get authorId(): string {
+    return this.properties.authorId;
+  }
+}
