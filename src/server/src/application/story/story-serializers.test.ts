a43b9116fd1fb2de42852a228da7aea20aabd247
evergosu <evergosu@gmail.com>
2026-01-23 16:00:04 +0300

    Added tests for story serializer.

---
+++ src/server/src/application/story/story-serializers.test.ts
@@ -0,0 +1,78 @@
+import type { StorySelectSchema } from 'server/database/schema/story';
+
+import {
+  StoryInsertSerializer,
+  StoryUpdateSerializer,
+} from './story-serializers';
+import { Story } from './story';
+
+describe('story serializers', () => {
+  const createdAt = new Date();
+
+  const TWENTY_FOUR_HOURS = 1000 * 60 * 60 * 24;
+
+  const expiresAt = new Date(createdAt.getTime() + TWENTY_FOUR_HOURS);
+
+  const raw: StorySelectSchema = {
+    authorId: '00000000-0000-0000-0000-000000000001',
+    id: '00000000-0000-0000-0000-000000000002',
+    body: 'This is the body',
+    title: 'Test story',
+    version: 0,
+    createdAt,
+    expiresAt,
+  };
+
+  const story = Story.rehydrate(raw);
+
+  function shouldSerializeStoryToDTO(
+    serializer: StoryInsertSerializer | StoryUpdateSerializer,
+  ) {
+    it('should serialize story to dto', () => {
+      const dtoResult = serializer.serialize(story.value);
+
+      expect(dtoResult.isSuccess()).toBe(true);
+
+      const dto = dtoResult.value;
+
+      expect(dto.id).toBe(raw.id);
+      expect(dto.authorId).toBe(raw.authorId);
+      expect(dto.title).toBe(raw.title);
+      expect(dto.body).toBe(raw.body);
+    });
+  }
+
+  function shouldSerializeListOfStoriesToDTOs(
+    serializer: StoryInsertSerializer | StoryUpdateSerializer,
+  ) {
+    it('should serialize array of stories to dtos array', () => {
+      const dtoResult = serializer.serializeList([story.value, story.value]);
+
+      expect(dtoResult.isSuccess()).toBe(true);
+
+      const dto = dtoResult.value;
+
+      expect(dto.length).toBe(2);
+
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const dtoSnd = dtoResult.value.at(1)!;
+
+      expect(dtoSnd.id).toBe(raw.id);
+      expect(dtoSnd.authorId).toBe(raw.authorId);
+      expect(dtoSnd.title).toBe(raw.title);
+      expect(dtoSnd.body).toBe(raw.body);
+    });
+  }
+
+  describe('insert', () => {
+    shouldSerializeStoryToDTO(new StoryInsertSerializer());
+
+    shouldSerializeListOfStoriesToDTOs(new StoryInsertSerializer());
+  });
+
+  describe('update', () => {
+    shouldSerializeStoryToDTO(new StoryUpdateSerializer());
+
+    shouldSerializeListOfStoriesToDTOs(new StoryUpdateSerializer());
+  });
+});
