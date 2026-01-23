61873ffecccfc12b4bb10778bfa946052cb4cccb
evergosu <evergosu@gmail.com>
2026-01-23 16:25:07 +0300

    Added version control to aggregate roots, added evolve method to support safe updates.

--- src/server/src/library/ddd/domain/aggregate-root.ts
+++ src/server/src/library/ddd/domain/aggregate-root.ts
@@ -4,28 +4,39 @@ import { type DomainEvent, DomainEvents } from './domain-events';
 import { Entity } from './entity';
 
 /**
+ * ---
  * Abstract base class for aggregate roots in a DDD system.
+ * ---
  * @template T - The type of the aggregate's properties.
  */
 export abstract class AggregateRoot<T> extends Entity<T> {
+  protected readonly _version: number;
   /**
+   * ---
    * Constructs a new `AggregateRoot` instance.
+   * ---
    * @param properties - The entity's domain properties.
    * @param id - An optional pre-defined unique identifier. If not provided, a new one will be generated.
+   * @param version - Incremental number, stored to control optimistic locking for concurrent modifications.
    */
-  constructor(properties: T, id?: UniqueIdentifier) {
+  constructor(properties: T, id?: UniqueIdentifier, version = 0) {
     super(properties, id, false);
     this._domainEvents = [];
+    this._version = version;
 
     Object.freeze(this);
   }
+
   /**
+   * ---
    * The internal collection of domain events associated with this aggregate.
    */
   private _domainEvents: DomainEvent[] = [];
 
   /**
+   * ---
    * Shallow copy of associated aggregate domain events.
+   * ---
    * @returns shallow copy of current aggregate domain events.
    */
   public get domainEvents(): DomainEvent[] {
@@ -33,7 +44,19 @@ export abstract class AggregateRoot<T> extends Entity<T> {
   }
 
   /**
+   * ---
+   * Retrieves a numeric version of an aggregate.
+   * ---
+   * Represents amount of updates to control optimistic locking for concurrent modifications
+   */
+  public get version(): number {
+    return this._version;
+  }
+
+  /**
+   * ---
    * Adds a domain event to the internal list and marks this aggregate for dispatch.
+   * ---
    * @param event - The domain event to add.
    */
   protected addDomainEvent(event: DomainEvent): void {
@@ -43,9 +66,36 @@ export abstract class AggregateRoot<T> extends Entity<T> {
   }
 
   /**
+   * ---
    * Clears all domain events.
    */
   public clearDomainEvents(): void {
     this._domainEvents.length = 0;
+  }
+
+  /**
+   * ---
+   * Creates a new version of this aggregate with updated properties.
+   * ---
+   * - identity is preserved
+   * - domain events are NOT copied
+   * ---
+   * @param overrides - Partial properties to override.
+   */
+  protected evolve(overrides: Partial<T>): this {
+    const ctor = this.constructor as new (
+      properties: T,
+      id: UniqueIdentifier,
+      version: number,
+    ) => this;
+
+    return new ctor(
+      {
+        ...this.properties,
+        ...overrides,
+      } as T,
+      this.id,
+      this._version + 1,
+    );
   }
 }
