bcc22596b4ba7c109a1e5b3687a3fe08615b464e
evergosu <evergosu@gmail.com>
2026-01-27 14:12:24 +0300

    Created GetAll capability to retrieve all records from repository.

---
+++ src/server/src/library/ddd/infrastructure/repository/capabilities/get-all.ts
@@ -0,0 +1,27 @@
+import type {
+  InferRehydratorFailure,
+  InferRehydratorDomain,
+  Rehydrator,
+  Task,
+} from 'server/library/ddd/primitives';
+import type { AggregateNotFoundFailure } from 'server/library/ddd/errors';
+
+/**
+ * ---
+ * Capability: Retrieve all aggregates from repository.
+ */
+export interface GetAll<R extends Rehydrator<unknown, unknown, unknown>> {
+  readonly rehydrator: R;
+
+  /**
+   * ---
+   * Retrieves all aggregates from repository.
+   * ---
+   * - fails if an aggregate does not exist in the persistance.
+   * - fails if an aggregate has failed rehydration step.
+   */
+  getAll(): Task<
+    InferRehydratorDomain<R>[],
+    InferRehydratorFailure<R>[] | AggregateNotFoundFailure
+  >;
+}
