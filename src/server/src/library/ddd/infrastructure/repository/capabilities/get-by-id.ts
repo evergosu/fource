213d1f2f8c690df97888f2a358a1139ac8e0e75e
evergosu <evergosu@gmail.com>
2026-01-30 16:20:56 +0300

    Created GetById capability to retrieve single entity by identifier.

---
+++ src/server/src/library/ddd/infrastructure/repository/capabilities/get-by-id.ts
@@ -0,0 +1,35 @@
+import type {
+  InferRehydratorFailure,
+  InferRehydratorDomain,
+  Rehydrator,
+  Entity,
+  Task,
+} from 'server/library/ddd/primitives';
+import type { AggregateNotFoundFailure } from 'server/library/ddd/errors';
+
+/**
+ * ---
+ * Capability: Retrieve an aggregate using provided unique identifier.
+ */
+export interface GetById<
+  Domain extends Entity<unknown>,
+  R extends Rehydrator<unknown, Domain, unknown>,
+> {
+  readonly rehydrator: R;
+
+  /**
+   * ---
+   * Retrieves an aggregate using provided unique identifier.
+   * ---
+   * - fails if an aggregate does not exist in the persistance.
+   * - fails if an aggregate has failed rehydration step.
+   * ---
+   * @param id - The unique identifier of the domain entity.
+   */
+  getById(
+    id: Domain['id'],
+  ): Task<
+    InferRehydratorDomain<R>,
+    InferRehydratorFailure<R> | AggregateNotFoundFailure
+  >;
+}
