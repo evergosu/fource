89b8964608552c61048fcb7e045ba322a1c1fa50
evergosu <evergosu@gmail.com>
2026-02-03 15:11:43 +0300

    Adapted repository connector, improved tests.

--- src/tests/src/features/story.spec.tsx
+++ src/tests/src/features/story.spec.tsx
@@ -1,47 +1,68 @@
-import {
-  NoStoriesError,
-  Stories,
-} from 'server/application/story/story-repository';
+import { StoryRepository } from 'server/application/story/story-repository';
 import { describeFeature, loadFeature } from '@amiceli/vitest-cucumber';
 import { connectRepository } from 'tests/library/connect-repository';
 import { render, screen } from '@testing-library/react';
+import { Story } from 'server/application/story/story';
 import { STEPS } from 'tests/steps';
 import Page from 'client/app/page';
 
 describeFeature(await loadFeature('./story.feature'), ({ Scenario }) => {
+  const story = Story.create({
+    authorId: 'John Doe',
+    body: 'The content',
+    title: 'The Story',
+  });
+
   Scenario('there is a story to suggest', ({ Given, When, Then }) => {
-    let stories: Stories;
-
     Given('a story to suggest is available', async (context: unknown) => {
-      stories = connectRepository(Stories, context);
+      const repository = connectRepository(StoryRepository, context);
 
-      await stories.create({ title: 'The story' });
+      await story
+        .toTask()
+        .flatMap(s => repository.create(s))
+        .run();
 
-      expect(await stories.getNext()).not.toBeNull();
+      const result = await story
+        .toTask()
+        .flatMap(s => repository.getById(s.id))
+        .run();
+
+      expect(result.value).not.toBeNull();
     });
 
     STEPS.aReaderVisitsTheHomePage(When);
 
-    Then('the story should be presented to the reader', async () => {
-      const story = await stories.getNext();
+    Then(
+      'the story should be presented to the reader',
+      async (context: unknown) => {
+        const repository = connectRepository(StoryRepository, context);
 
-      render(await Page());
+        const result = await story
+          .toTask()
+          .flatMap(s => repository.getById(s.id))
+          .run();
 
-      expect(screen.getByRole('main')).toHaveTextContent(story.title);
-    });
+        render(await Page());
+
+        expect(screen.getByRole('main')).toHaveTextContent(
+          result.value.title.title,
+        );
+      },
+    );
   });
 
   Scenario('there is no story to suggest', ({ Given, When, Then }) => {
-    let stories: Stories;
-
     Given('a story to suggest is not available', async (context: unknown) => {
-      stories = connectRepository(Stories, context);
+      const repository = connectRepository(StoryRepository, context);
 
-      await stories.drop();
+      await story
+        .toTask()
+        .flatMap(s => repository.delete(s.id))
+        .run();
 
-      await expect(() => stories.getNext()).rejects.toThrow(
-        new NoStoriesError(),
-      );
+      const result = await repository.getAll().run();
+
+      expect(result.isFailure()).toBeTruthy();
     });
 
     STEPS.aReaderVisitsTheHomePage(When);
--- src/tests/src/library/connect-repository.ts
+++ src/tests/src/library/connect-repository.ts
@@ -1,11 +1,16 @@
-import type { Repository } from 'server/application/story/story-repository';
+import type { Repository } from 'server/library/ddd/primitives';
 import type { TestContext } from 'vitest';
 
-export function connectRepository<T extends Repository>(
+import { PostgresErrorTranslator } from 'server/database/clients/postgres/postgres-error-translator';
+
+export function connectRepository<T extends Repository<unknown>>(
   repository: new (
     ...parameters: ConstructorParameters<typeof Repository>
   ) => T,
   context: unknown,
 ): T {
-  return new repository((context as TestContext).database);
+  return new repository(
+    (context as TestContext).database,
+    new PostgresErrorTranslator(),
+  );
 }
