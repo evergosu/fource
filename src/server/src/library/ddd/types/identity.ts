b75b6c76c307b64ac979a588ce9ed9fe7b11af05
evergosu <evergosu@gmail.com>
2026-02-17 11:56:10 +0300

    Replaced identity function with symbol to use as sentinel instead.

--- src/server/src/library/ddd/types/identity.ts
+++ src/server/src/library/ddd/types/identity.ts
@@ -1,14 +1,10 @@
 /**
  * ---
- * Identity function. Returns the value unchanged.
+ * Identity sentinel.
  * ---
  * Used to explicitly express "no transformation",
  * especially in widening and normalization flows.
- * ---
- * @param value - any value
- * ---
- * ```ts
- * identity(5) // 5
- * ```
  */
-export const identity = <T>(value: T): T => value;
+export const identity: unique symbol = Symbol('identity');
+
+export type Identity = typeof identity;
